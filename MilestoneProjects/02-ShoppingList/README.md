f628d8e8a9e6a74911e8cd425ec324178f620da6
josecarlosLH <josecarlos.lopez@internaliagroup.com>
2023-03-29 16:58:48 +0200

    Add milestone project 2
    q
    exit

---
+++ MilestoneProjects/02-ShoppingList/README.md
@@ -0,0 +1,56 @@
+# Shopping List
+
+[Milestone project 2](https://www.hackingwithswift.com/guide/3/1) from the [100 Days of Swift course](https://www.hackingwithswift.com/100) by [Hacking With Swift](https://www.hackingwithswift.com/).
+
+## Contents
+
+|                      Day                      | Contents                                                                                                                                                                                                          |
+|:---------------------------------------------:|:------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| [32](https://www.hackingwithswift.com/100/32) | <ul><li>[What you learned](https://www.hackingwithswift.com/guide/3/1)</li><li>[Key points](https://www.hackingwithswift.com/guide/3/2)</li><li>[Challenge](https://www.hackingwithswift.com/guide/3/3)</li></ul> |
+
+## Notes
+
+- I have added these additional features to the app:
+  - Let the users edit an item of the list by tapping on it.
+  - Add share option to the `navigationItem`.
+  - Add swipe-to-delete functionality for each `tableView`'s row.
+
+## Challenge Instructions
+
+*Instructions taken from [here](https://www.hackingwithswift.com/guide/3/3).* 
+
+It’s time to put your skills to the test by making your own complete app from scratch. This time your job is to create an app that lets people create a shopping list by adding items to a table view.
+
+The best way to tackle this app is to think about how you build project 5: it was a table view that showed items from an array, and we used a UIAlertController with a text field to let users enter free text that got appended to the array. That forms the foundation of this app, except this time you don’t need to validate items that get added – if users enter some text, assume it’s a real product and add it to their list.
+
+For bonus points, add a left bar button item that clears the shopping list – what method should be used afterwards to make the table view reload all its data?
+
+Here are some hints in case you hit problems:
+
+- Remember to change `ViewController` to build on `UITableViewController`, then change the storyboard to match.
+- Create a `shoppingList´ property of type `[String]` to hold all the items the user wants to buy.
+- Create your `UIAlertController` with the style `.alert`, then call `addTextField()` to let the user enter text.
+- When you have a new shopping list item, make sure you `insert()` it into your `shoppingList` array before you call the `insertRows(at:)` method of your table view – your app will crash if you do this the wrong way around.
+
+You might be tempted to try to use UIActivityViewController to share the finished shopping list by email, but if you do that you’ll hit a problem: you have an array of strings, not a single string.
+
+There’s a special method that can create one string from an array, by stitching each part together using a separator you provide. I’ll be going into it in project 8, but if you’re keen to try it now here’s some code to get you started:
+
+```Swift
+let list = shoppingList.joined(separator: "\n")
+```
+
+That will create a new `list` constant that is a regular string, with each shopping list item separated by “\n” – that’s Swift’s way of representing a new line.
+
+## Screenshots
+
+<div align="center">
+  <img src="./Screenshots/1.png" alt="Main screen" width="325">
+  <img src="./Screenshots/2.png" alt="Add action" width="325">
+  <img src="./Screenshots/3.png" alt="Share action" width="325">
+</div>
+
+<div align="center">
+  <img src="./Screenshots/4.png" alt="Edit action" width="325">
+  <img src="./Screenshots/5.gif" alt="Delete action" width="325">
+</div>
---
+++ MilestoneProjects/02-ShoppingList/ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,152 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by JC on 29/3/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    // MARK: Properties
+    
+    private var items = [String]()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViewController()
+    }
+    
+    // MARK: - Methods
+
+    private func configureViewController() {
+        title = "Shopping List"
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(
+                barButtonSystemItem: .action,
+                target: self,
+                action: #selector(didTapShare)
+            ),
+            UIBarButtonItem(
+                barButtonSystemItem: .add,
+                target: self,
+                action: #selector(didTapAdd)
+            )
+        ]
+                
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .trash,
+            target: self,
+            action: #selector(didTapTrash)
+        )
+    }
+    
+    /**
+     Prompt a `UIAlertController` with a textfield.
+     
+     - Parameters:
+        - title: UIAlertController's title.
+        - item: The selected item to edit. `nil` if the user is creating a new item instead.
+        - completion: The block to execute when the user taps on the `Submit` action. This block
+        has no return value and takes the value of the textfield as a parameter.
+     */
+    private func promptTextfield(title: String, item: String? = nil, completion: @escaping (String) -> Void) {
+        let alertController = UIAlertController(
+            title: title,
+            message: nil,
+            preferredStyle: .alert
+        )
+        alertController.addTextField()
+        alertController.textFields?[0].text = item
+        
+        alertController.addAction(UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak alertController] _ in
+            
+            guard let self = self, let alertController = alertController else { return }
+            
+            guard let answer = alertController.textFields?[0].text else { return }
+            completion(answer)
+            self.tableView.reloadData()
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(alertController, animated: true)
+    }
+    
+    // MARK: Actions
+    
+    @objc private func didTapShare() {
+        let itemsString = items.isEmpty ? "" : "- " + items.joined(separator: "\n- ")
+        
+        let viewController = UIActivityViewController(
+            activityItems: [itemsString],
+            applicationActivities: []
+        )
+
+        /**
+         This line is for iPad, because the activity view controller must be shown from somewhere on
+         the screen. In this case, we are telling the view that it will pop up from a bar button item,
+         which is the right bar button item.
+         */
+        viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+
+        present(viewController, animated: true)
+    }
+    
+    @objc private func didTapTrash() {
+        items.removeAll()
+        tableView.reloadData()
+    }
+
+    @objc private func didTapAdd() {
+        promptTextfield(title: "Enter shopping list item") { answer in
+            self.items.append(answer)
+        }
+    }
+    
+}
+
+
+// MARK: - Data Source
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        promptTextfield(title: "Edit shopping list item", item: items[indexPath.row]) { answer in
+            self.items[indexPath.row] = answer
+        }
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        if editingStyle == .delete {
+            // Remove the item from the data source
+            items.remove(at: indexPath.row)
+
+            // Delete the row from the table view
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+}
