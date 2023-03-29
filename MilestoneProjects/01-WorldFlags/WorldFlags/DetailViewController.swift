7f82f1357feaf4db383099937cd21056ea0db61f
josecarlosLH <josecarlos.lopez@internaliagroup.com>
2023-03-29 02:44:00 +0200

    Add project 1 of milestone projects

---
+++ MilestoneProjects/01-WorldFlags/WorldFlags/DetailViewController.swift
@@ -0,0 +1,186 @@
+//
+//  DetailViewController.swift
+//  WorldFlags
+//
+//  Created by JC on 28/3/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var countryName: String!
+    var imageFilename: String!
+    
+    private var areNavigationBarViewsHidden = false
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViewController()
+        configureTapGestureRecognizer()
+        configurePinchGestureRecognizer()
+        configurePanGesture()
+        print(imageView.image!.size.width)
+        print(imageView.image!.size.height)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    // MARK: - Methods
+    
+    private func configureViewController() {
+        title = countryName
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        
+        imageView.image = UIImage(named: imageFilename)
+        imageView.isUserInteractionEnabled = true
+    }
+                     
+    private func configureTapGestureRecognizer() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    private func configurePinchGestureRecognizer() {
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(didPinch(_:)))
+        imageView.addGestureRecognizer(pinchGesture)
+    }
+    
+    private func configurePanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        imageView.addGestureRecognizer(panGesture)
+    }
+    
+    // MARK: Actions
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        areNavigationBarViewsHidden.toggle()
+
+        if areNavigationBarViewsHidden {
+            // Hide navigation bar items
+            navigationController?.navigationBar.subviews.forEach { subview in
+                subview.isHidden = true
+            }
+
+            // Animate background color change to black
+            UIView.transition(with: view, duration: 0.25, options: .transitionCrossDissolve, animations: {
+                self.view.backgroundColor = .black
+            })
+        } else {
+            // Show navigation bar items
+            navigationController?.navigationBar.subviews.forEach { subview in
+                subview.isHidden = false
+            }
+
+            // Animate background color change to white
+            UIView.transition(with: view, duration: 0.25, options: .transitionCrossDissolve, animations: {
+                self.view.backgroundColor = .white
+            })
+        }
+    }
+    
+    @objc private func didTapShare() {
+        // activityItems is the data to be shared. selectedItem! will set the name of the file in the
+        // sharing pop up, as requested by the challenge of the instructor.
+        let viewController = UIActivityViewController(
+            activityItems: [imageView.image!],
+            applicationActivities: []
+        )
+
+        /**
+         This line is for iPad, because the activity view controller must be shown from somewhere on
+         the screen. In this case, we are telling the view that it will pop up from a bar button item,
+         which is the right bar button item.
+         */
+        viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+
+        // Same as show()
+        present(viewController, animated: true)
+    }
+    
+    @objc private func didPinch(_ sender: UIPinchGestureRecognizer) {
+        if sender.state == .began || sender.state == .changed {
+            // Scale the image view by the gesture's scale
+            imageView.transform = imageView.transform.scaledBy(x: sender.scale, y: sender.scale)
+            
+            // Limit the minimum scale value
+            let minScale: CGFloat = 1.0
+            let currentScale = imageView.frame.size.width / imageView.bounds.size.width
+            let newScale = currentScale * sender.scale
+            if newScale < minScale {
+                sender.scale = minScale / currentScale
+                imageView.transform = imageView.transform.scaledBy(
+                    x: minScale / currentScale,
+                    y: minScale / currentScale
+                )
+            }
+            
+            sender.scale = 1.0
+        }
+    }
+    
+    /** How to prevent a zoomed in image with pan gesture attached from panning outside the screen’s frame?
+     
+     @objc private func didPan(_ sender: UIPanGestureRecognizer) {
+         let translation = sender.translation(in: imageView.superview)
+         
+         if sender.state == .began || sender.state == .changed {
+             let maxX: CGFloat = imageView.image!.size.width
+             let minX: CGFloat = 0
+             let maxY: CGFloat = imageView.image!.size.height
+             let minY: CGFloat = 0
+             
+             var newCenterX = imageView.center.x + (translation.x)
+             var newCenterY = imageView.center.y + translation.y
+             
+             if newCenterX - translation.x < minX {
+                 newCenterX = 0
+             }
+             
+             if newCenterX + translation.x > maxX {
+                 newCenterX = maxX
+             }
+             
+             print("newCenterX: \(newCenterX)")
+             
+             print("translation.x: \(translation.x)")
+             print("translation.y: \(translation.y)")
+             
+             // Update the image view's position based on the gesture's translation
+             imageView.center = CGPoint(x: newCenterX, y: newCenterY)
+             sender.setTranslation(CGPoint.zero, in: imageView.superview)
+         }
+     }
+     */
+    
+    @objc func didPan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: imageView.superview)
+        
+        if sender.state == .began || sender.state == .changed {
+            // Update the image view's position based on the gesture's translation
+            imageView.center = CGPoint(
+                x: imageView.center.x + translation.x,
+                y: imageView.center.y + translation.y
+            )
+            sender.setTranslation(CGPoint.zero, in: imageView.superview)
+        }
+    }
+
+}
---
+++ MilestoneProjects/01-WorldFlags/WorldFlags/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  WorldFlags
+//
+//  Created by JC on 27/3/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    // MARK: Properties
+    
+    private var countryFlagDict = [[String : String]]()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "World Flags"
+        tableView.rowHeight = 65
+        
+        loadImages()
+    }
+    
+    // MARK: - Methods
+    
+    private func loadImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let filenames = try! fm.contentsOfDirectory(atPath: path)
+        
+        filenames.forEach { filename in
+            if filename.hasSuffix("-sm.png") {
+                let locale = Locale(identifier: Locale.current.languageCode ?? "en_US")
+                if let countryName = locale.localizedString(
+                    forRegionCode: filename.replacingOccurrences(of: "-sm.png", with: "")
+                ) {
+                    countryFlagDict.append([countryName : filename])
+                }
+            }
+        }
+        
+        countryFlagDict = countryFlagDict.sorted {
+            $0.keys.first!.localizedCaseInsensitiveCompare($1.keys.first!) == .orderedAscending
+        }
+    }
+    
+    private func scale(image: UIImage, to size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!.withRenderingMode(.alwaysOriginal)
+    }
+    
+}
+
+// MARK: - Data Source
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countryFlagDict.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        let country = countryFlagDict[indexPath.row]
+        
+        cell.textLabel?.text = country.keys.first!
+        cell.imageView?.image = scale(
+            image: UIImage(named: country.values.first!)!,
+            to: CGSize(width: 65, height: 40)
+        )
+        
+        // To avoid flags with white color to blend in with the bachgorund
+        cell.imageView?.layer.borderColor = UIColor.gray.cgColor
+        cell.imageView?.layer.borderWidth = 1
+        
+        // Set the accessory type to an arrow
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let country = countryFlagDict[indexPath.row]
+        navigateToDetail(countryName: country.keys.first!, imageFilename: country.values.first!)
+    }
+
+}
+
+// MARK: - Navigation
+extension ViewController {
+ 
+    private func navigateToDetail(countryName: String, imageFilename: String) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.countryName = countryName
+            vc.imageFilename = imageFilename.replacingOccurrences(of: "-sm", with: "-lg")
+            
+            show(vc, sender: self)
+        }
+    }
+    
+}
